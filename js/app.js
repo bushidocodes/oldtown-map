da949fbc73364495cb561de89e51c74c8ea2e0b9
Sean McBride <spmcbride1201@gmail.com>
2016-06-24 15:37:04 -0400

    feat: Implement data model and intial view model and dynamically populate nav bar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+//Site Class
+class Site {
+    constructor(name, address, description){
+        var self = this;
+        self.name = name;
+        self.address = address;
+        self.description = description;
+    };
+};
 
 //Google Maps Functionality
 var map;
@@ -8,3 +17,31 @@ map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15
 });
 }
+
+//Knockout Functionality
+var ViewModel = function () {
+    var self = this;
+    self.sites = ko.observableArray([
+        new Site("Ramsay House","",""),
+        new Site("Carlyle House","",""),
+        new Site("Gadsby's Tavern","",""),
+        new Site("Spite House","",""),
+        new Site("Revolutionary War Cobblestones","",""),
+        new Site("Robert E. Lee's Boyhood Home","",""),
+        new Site("Lee-Fendall House","",""),
+        new Site("Lloyd House","",""),
+        new Site("Christ Church","",""),
+        new Site("Friendship Firehouse","",""),
+        new Site("The Lyceum","",""),
+        new Site("The Confederate Soldier","",""),
+        new Site("Market Square","",""),
+        new Site("Stabler-Leadbeater Apothecary Shop","",""),
+        new Site("Old Presbyterian Meeting House","",""),
+        new Site("Gentry Row","",""),
+        new Site("The Athenaeum","",""),
+        new Site("Captain's Row","",""),
+        new Site("Torpedo Factory","",""),
+    ]);
+};
+
+ko.applyBindings(new ViewModel());
