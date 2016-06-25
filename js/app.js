bb2a34d2a27e259210788438ebd74a87f8fedfc2
Sean McBride <spmcbride1201@gmail.com>
2016-06-25 11:23:42 -0400

    feat: Hardcode lat and lng into data model and render pins and info windows

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-//Referenced http://jsfiddle.net/schmidlop/5eTRV/10/
-
 //Custom Classes
 $(document).ready(function () {
+
     class Site {
-        constructor(name, address, description) {
+        constructor(name, address, lat, lng, description) {
             var self = this;
             self.name = name;
             self.address = address;
+            self.lat = lat;
+            self.lng = lng;
             self.description = description;
         };
     };
@@ -16,76 +17,55 @@ $(document).ready(function () {
         geocoder = new google.maps.Geocoder();
         self.map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: 38.805, lng: -77.05 },
-            zoom: 15,
+            zoom: 16,
             mapTypeId: google.maps.MapTypeId.SATELLITE
         });
-        // map = new google.maps.Map(document.getElementById('map'), {
-        //     center: { lat: 38.805, lng: -77.05 },
-        //     zoom: 15,
-        //     mapTypeId: google.maps.MapTypeId.SATELLITE
-        // });
-
-        var marker = new google.maps.Marker({
-            position: { lat: 38.805, lng: -77.05 },
-            map: map,
-            title: 'Hello World!'
-        });
-
-        // var marker = new google.maps.Marker({
-        //     position: { vm.sites()[0].addresslat: 38.805, lng: -77.05 },
-        //     map: map,
-        //     title: 'Hello World!'
-        // });
 
         for (var i = 0; i < vm.sites().length; i++) {
-            console.log(vm.sites()[i].address);
-            geocoder.geocode({ 'address': vm.sites()[i].address }, function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    // map.setCenter(results[0].geometry.location);
-                    console.log("lat is " + results[0].geometry.location.lat());
-                    console.log("lng is " + results[0].geometry.location.lng());
-                    var marker = new google.maps.Marker({
-                        map: map,
-                        position: results[0].geometry.location
-                    });
-                } else {
-                    alert("Geocode was not successful for the following reason: " + status);
-                }
+            let marker = new google.maps.Marker({
+                position: { lat: vm.sites()[i].lat, lng: vm.sites()[i].lng },
+                map: map,
+                title: vm.sites()[i].name
             });
+            let contentString = '<h3>' + vm.sites()[i].name + '</h3>' + '<p>'+ vm.sites()[i].description +'</p>';
+            let infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+
         }
     }
-
-
     //Knockout Functionality
     var ViewModel = function () {
         // Data
         var self = this;
         //site pulled from Frommer's at http://www.frommers.com/destinations/alexandria-va/623095
         self.sites = ko.observableArray([
-            new Site("Ramsay House", "221 King St, Alexandria, VA 22314", ""),
-            new Site("Carlyle House", "121 N Fairfax St, Alexandria, VA 22314", ""),
-            new Site("Gadsby's Tavern", "134 N Royal St, Alexandria, VA 22314", ""),
-            new Site("523 Queen St, Alexandria, VA 22314", "", ""),
-            new Site("Revolutionary War Cobblestones", "607 Princess St, Alexandria, VA 22314", ""),
-            new Site("Robert E. Lee's Boyhood Home", "607 Oronoco Street, Alexandria, Virginia 22314", ""),
-            new Site("Lee-Fendall House", "614 Oronoco St, Alexandria, VA 22314", ""),
-            new Site("Lloyd House", "220 N Washington St, Alexandria, VA 22314", ""),
-            new Site("Christ Church", "118 North Washington St, Alexandria, Virginia 22314", ""),
-            new Site("Friendship Firehouse", "107 S Alfred St, Alexandria, VA 22314", ""),
-            new Site("The Lyceum", "201 S Washington St, Alexandria, VA 22314", ""),
-            new Site("Appomattox Statue", "201 S Washington St, Alexandria, VA 22314", ""),
-            new Site("Market Square", "301 King St, Alexandria, VA 22314", ""),
-            new Site("Stabler-Leadbeater Apothecary Shop", "107 S Fairfax St, Alexandria, VA 22314", ""),
-            new Site("Old Presbyterian Meeting House", "323 S Fairfax St, Alexandria, VA 22314", ""),
-            new Site("Gentry Row", "212 Prince St, Alexandria, VA 22314", ""),
-            new Site("The Athenaeum", "201 Prince St, Alexandria, VA 22314", ""),
-            new Site("Captain's Row", "115 Prince St, Alexandria, VA 22314", ""),
-            new Site("Torpedo Factory", "105 N Union St, Alexandria, VA 22314", ""),
+            new Site("Ramsay House", "221 King St, Alexandria, VA 22314", 38.804628, -77.042357, ""),
+            new Site("Carlyle House", "121 N Fairfax St, Alexandria, VA 22314", 38.805228, -77.042012, ""),
+            new Site("Gadsby's Tavern", "134 N Royal St, Alexandria, VA 22314", 38.805554, -77.043619, ""),
+            new Site("Spite House", "523 Queen St, Alexandria, VA 22314", 38.807307, -77.045064, ""),
+            new Site("Revolutionary War Cobblestones", "607 Princess St, Alexandria, VA 22314", 38.808553, -77.045503, ""),
+            new Site("Robert E. Lee's Boyhood Home", "607 Oronoco Street, Alexandria, Virginia 22314", 38.809808, -77.045208, ""),
+            new Site("Lee-Fendall House", "614 Oronoco St, Alexandria, VA 22314", 38.809437, -77.045725, ""),
+            new Site("Lloyd House", "220 N Washington St, Alexandria, VA 22314", 38.807214, -77.046751, ""),
+            new Site("Christ Church", "118 North Washington St, Alexandria, Virginia 22314", 38.806271, -77.047460, ""),
+            new Site("Friendship Firehouse", "107 S Alfred St, Alexandria, VA 22314", 38.804955, -77.049491, ""),
+            new Site("The Lyceum", "201 S Washington St, Alexandria, VA 22314", 38.803759, -77.047537, ""),
+            new Site("Appomattox Statue", "201 S Washington St, Alexandria, VA 22314", 38.803911, -77.047210, ""),
+            new Site("Market Square", "301 King St, Alexandria, VA 22314", 38.805406, -77.042894, ""),
+            new Site("Stabler-Leadbeater Apothecary", "107 S Fairfax St, Alexandria, VA 22314", 38.804238, -77.042773, ""),
+            new Site("Old Presbyterian Meeting House", "323 S Fairfax St, Alexandria, VA 22314", 38.801352, -77.043419, ""),
+            new Site("Gentry Row", "212 Prince St, Alexandria, VA 22314", 38.803181, -77.042304, ""),
+            new Site("The Athenaeum", "201 Prince St, Alexandria, VA 22314", 38.803438, -77.041821, ""),
+            new Site("Captain's Row", "115 Prince St, Alexandria, VA 22314", 38.803433, -77.041098, ""),
+            new Site("Torpedo Factory", "105 N Union St, Alexandria, VA 22314", 38.804892, -77.039802, ""),
         ]);
     };
     var vm = new ViewModel;
 
     ko.applyBindings(vm);
-
 
 });
