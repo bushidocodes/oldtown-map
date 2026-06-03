2ce8e7b2a207d509721f3e3ff19ac8982e6cd812
bushidocodes <bushidocodes@users.noreply.github.com>
2026-06-03 16:11:35 +0000

    deploy: ab0f98f44fa14ef5133e793ca68ac47caf0787a6

--- js/scripts.js
+++ js/scripts.js
@@ -433,6 +433,7 @@ var ViewModel = function ViewModel() {
     clearInfoWindow();
     //Don't close the sidebar automatically if the window is so wide that the infowindow is fully visible on the visible portion of the map div
     if ($(window).width() < 1500) {
+      hamburger_cross();
       $("#wrapper").toggleClass("toggled");
     }
     map.panTo({
