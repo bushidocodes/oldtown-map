e4df14108a33f2c53fb96148ad8b77fc8298c307
bushidocodes <bushidocodes@users.noreply.github.com>
2026-06-10 11:16:57 +0000

    deploy: 977aecc6db93bf42ff1b1c399902d499c390a46e

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
-"use strict";
+'use strict';
 
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
-function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
-function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
 /**
 * @description Represents a site in Old Town Alexandria
 * @constructor
@@ -16,6 +10,12 @@ function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Can
 * @param {string} description - A description of the site
 * @param {string} wikipediaID - The unique wikipedia ID of the site if it has a Wikipedia page. Or null if it does not.
 */
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
+function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
 var Site = /*#__PURE__*/_createClass(function Site(name, address, lat, lng, description, wikipediaID) {
   _classCallCheck(this, Site);
   this.name = name;
@@ -33,6 +33,7 @@ var Site = /*#__PURE__*/_createClass(function Site(name, address, lat, lng, desc
 // highlightenIcon is a dynamically generated (during initMap()) custom icon that is used for markers when they are
 // hovered over (via the marker itself or via the sidebar)
 var infoWindow = null;
+var map = null;
 var markers = ko.observableArray();
 var defaultIcon;
 var highlightedIcon;
@@ -163,7 +164,7 @@ var styles = [{
 
 function initMap() {
   // Declare and instatiate Google Map object using desired view zoom, custom styling and map type
-  self.map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {
       lat: 38.806,
       lng: -77.045
